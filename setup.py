a64fb7a4fc7e09b29f342f4ef9b4517b2088aa43
sludds <asludds@mit.edu>
2017-07-20 13:55:25 -0400

    Upgrading package to be ready for PyPI

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'xyztostl',
+  packages = ['xyztostl'], # this must be the same as the name above
+  version = '0.1',
+  description = 'A library that takes points clouds from different file types and converts them into an stl file for printing as best as possible.',
+  author = 'Alexander Sludds',
+  author_email = 'asludds@mit.edu',
+  url = 'https://github.com/alexsludds/xyztostl', # use the URL to the github repo
+  download_url = 'https://github.com/alexsludds/xyztostl/archive/0.1.tar.gz', # I'll explain this in a second
+  keywords = ['printing', 'stl', 'points'], # arbitrary keywords
+  classifiers = [],
+)
